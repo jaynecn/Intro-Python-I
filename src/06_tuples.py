e3e6ff4ec62daca7cb0fecfb44ce426064709f2f
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2020-02-10 19:34:52 +0000

    slices.py

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -17,12 +17,14 @@ automatically.
 
 import math
 
+
 def dist(a, b):
     """Compute the distance between two x,y points."""
     x0, y0 = a  # Destructuring assignment
     x1, y1 = b
 
     return math.sqrt((x1 - x0)**2 + (y1 - y0)**2)
+
 
 a = (2, 7)   # <-- x,y coordinates stored in tuples
 b = (-14, 72)
@@ -31,12 +33,22 @@ b = (-14, 72)
 print("Distance is: {:.2f}".format(dist(a, b)))
 
 
-
 # Write a function `print_tuple` that prints all the values in a tuple
 
 # YOUR CODE HERE
 
 t = (1, 2, 5, 7, 99)
+
+# I FOUND THIS DIFFICULT
+def print_tuple(t):
+    while 1 < len(t):
+        print(t[i])
+        i += 1
+        
+#  for n in t:
+#     print(n)
+
+
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
