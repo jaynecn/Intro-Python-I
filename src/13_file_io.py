fd312cb8fcf00cc7d53870e007bcecc1d1d84f81
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2020-04-07 13:00:31 +0100

    finally worked out file_io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,24 +9,29 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
-# with open('src/test.txt', 'r') as reader:
-#   print(reader.read())
-  
-# with open('bar.txt') as f:
-#   f.read()
 
 import os
 print(os.getcwd())
 
-# dir = r'C:\Users\Jayne\Documents\LAMBDA\Computer Science\Week 1 - Python\Intro-Python-I\src'
 
-# print(os.chdir(dir))
+with open('src/foo.txt') as foo:
+    text_doc = foo.read()
+
+print(text_doc)
+
+print('----------------------------------------------------')
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# with open('bar.txt') as b:
-#   read_bar = b.read()
-#   print(read_bar)
+
+with open('src/bar.txt', 'w') as bar:
+    write_new = bar.write('O Flower of Scotland\nWhen will we see \nyour like again?')
+
+with open('src/bar.txt') as new_bar:
+    read_bar_doc = new_bar.read()
+
+print(read_bar_doc) 
